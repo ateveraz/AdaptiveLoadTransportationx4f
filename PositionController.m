d21d6715f8d292bb50dd45664c638f334d317690
ateveraz <alejandro.tevera@cinvestav.mx>
2025-11-13 15:19:31 +0100

    Raw implementation of PID in position

---
+++ PositionController.m
@@ -0,0 +1,43 @@
+classdef PositionController < matlab.System
+    % Controller Matlab system object for an attitude controller.
+
+    properties (Access = public)
+        Kp = 1.0;
+        Kd = 2.0;
+        Ki = 0.5;
+    end
+
+    methods (Access = protected)
+        function setupImpl(~)
+        end
+
+        function [u, up] = stepImpl(obj, e, ep, int_e)
+            u = - obj.Kp * e - obj.Kd * ep - obj.Ki * int_e;
+            up = - obj.Kp * ep - obj.Ki * e;
+        end
+
+        function resetImpl(~)
+            % Initialize / reset internal properties
+        end
+
+        function [u, up] = getOutputSizeImpl(~)
+            u = [3 1];
+            up = [3 1];
+        end
+
+        function [u, up] = getOutputDataTypeImpl(~)
+            u = 'double';
+            up = 'double';
+        end
+
+        function [u, up] = isOutputComplexImpl(~)
+            u = false;
+            up = false;
+        end
+
+        function [u, up] = isOutputFixedSizeImpl(~)
+            u = true;
+            up = true;
+        end
+    end
+end
