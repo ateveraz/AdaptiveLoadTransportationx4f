0e3a60d9c1606f8383bdd8ad83f81d3ad35a2fe3
ateveraz <alejandro.tevera@cinvestav.mx>
2025-11-14 00:17:29 +0100

    Preliminar RBF implementation as compensator

--- PositionController.m
+++ PositionController.m
@@ -1,48 +1,71 @@
 classdef PositionController < matlab.System
-    % Controller Matlab system object for an attitude controller.
+    % PositionController Controlador de posición con RBFNN
 
     properties (Access = public)
-        Kp = 1.0;
         Kd = 2.0;
-        Ki = 0.5;
+        alpha = 1.0;
         mass = 0.4;
+        Gamma = 0.1;
+        kappa = 0.01;
+    
+        N_neurons = 10;
+        center_range = 1.0;
+        eta_width = 1.5;
     end
 
     properties (Access = private)
         gravity = [0 0 9.81]';
+        nn; 
+        dim_in  = 6; 
+        dim_out = 3;
     end
 
     methods (Access = protected)
-        function setupImpl(~)
+        function setupImpl(obj)
+            mu = (rand(obj.dim_in, obj.N_neurons) * 2 - 1) * obj.center_range;
+            eta = obj.eta_width;
+            obj.nn = RBFNN(mu, eta, obj.Gamma, obj.kappa, obj.dim_out);
         end
 
-        function [u, up] = stepImpl(obj, e, ep, int_e)
-            u = - obj.Kp * e - obj.Kd * ep - obj.Ki * int_e + obj.mass * obj.gravity;
-            up = - obj.Kp * ep - obj.Ki * e;
+        function [u, up, rbf, wp] = stepImpl(obj, xLdpp, e, ep, w) 
+            s = ep + obj.alpha * e;
+            x = [e; ep];
+
+            [rbf, wp] = obj.nn.compute_update(s, w, x);
+            u = obj.mass * (obj.gravity + xLdpp + obj.alpha*ep) - obj.Kd * s;
+            
+            up = zeros(3,1); % Fix it ? 
         end
 
         function resetImpl(~)
-            % Initialize / reset internal properties
         end
 
-        function [u, up] = getOutputSizeImpl(~)
-            u = [3 1];
-            up = [3 1];
+        function [u, up, rbf, wp] = getOutputSizeImpl(obj)
+            u  = [obj.dim_out 1];
+            up = [obj.dim_out 1];
+            rbf = [obj.dim_out 1];
+            wp = [obj.dim_out obj.N_neurons];
         end
 
-        function [u, up] = getOutputDataTypeImpl(~)
+        function [u, up, rbf, wp] = getOutputDataTypeImpl(~)
             u = 'double';
             up = 'double';
+            rbf = 'double';
+            wp = 'double';
         end
 
-        function [u, up] = isOutputComplexImpl(~)
+        function [u, up, rbf, wp] = isOutputComplexImpl(~)
             u = false;
             up = false;
+            rbf = false;
+            wp = false;
         end
 
-        function [u, up] = isOutputFixedSizeImpl(~)
+        function [u, up, rbf, wp] = isOutputFixedSizeImpl(~)
             u = true;
             up = true;
+            rbf = true;
+            wp = true;
         end
     end
 end
---
+++ RBFNN.m
@@ -0,0 +1,52 @@
+classdef RBFNN
+    %RBFNN Red Neuronal de Función de Base Radial (Corregida)
+    %   Aproxima una función f(z): R^m -> R^k
+    %   En este caso, z es 6x1 (estado de error) y la salida es 3x1 (fuerza)
+
+    properties
+        mu    % Centros (Matriz [dim_in x N])
+        eta   % Anchos (Vector [N x 1])
+        Gamma % Tasa de Aprendizaje (Matriz 3x3 diagonal o escalar)
+        kappa % Factor de amortiguamiento para aprendizaje robusto (escalar)
+        
+        N       % Número de neuronas (escalar)
+        dim_in  % Dimensión de la entrada (escalar, ej. 6)
+        dim_out % Dimensión de la salida (escalar, ej. 3)
+    end
+
+    methods (Access = public)
+        function obj = RBFNN(mu, eta, Gamma, kappa, dim_out)
+            obj.mu = mu;
+            [obj.dim_in, obj.N] = size(mu);
+            obj.dim_out = dim_out;
+            
+            if isscalar(eta)
+                obj.eta = repmat(eta, obj.N, 1);
+            else
+                assert(length(eta) == obj.N, 'Eta should have N-elements');
+                obj.eta = eta;
+            end
+            
+            obj.Gamma = Gamma;
+            obj.kappa = kappa;
+        end
+
+        function [nn_output, W_dot] = compute_update(obj, s, W, z)
+            sigma_vec = obj.compute_sigma(z);
+            nn_output = W * sigma_vec;
+            W_dot = obj.Gamma * (s * sigma_vec' - obj.kappa * norm(s) * W);
+        end
+
+        function sigma_vec = compute_sigma(obj, z)
+            sigma_vec = zeros(obj.N, 1);
+            z_col = z(:); 
+            
+            for j = 1:obj.N
+                mu_j = obj.mu(:, j);
+                eta_j = obj.eta(j);
+                dist_sq = sum((z_col - mu_j).^2);
+                sigma_vec(j) = exp(-dist_sq / (2 * eta_j^2));
+            end
+        end
+    end
+end
